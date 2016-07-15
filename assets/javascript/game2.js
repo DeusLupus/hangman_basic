3df460a43611343bf99690b348ce3bca89fcaaf2
Deo Bissessar <Deo Bissessar>
2016-07-15 07:34:14 -0400

    adding psychic game

---
+++ assets/javascript/game2.js
@@ -0,0 +1,6 @@
+var letter = ["a","b","c","d","e","f","g","h","i","j","k","l","m",
+			"n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+var rand = letter[Math.floor(Math.random() * letter.length)];
+console.log ("random index " + rand);
+
